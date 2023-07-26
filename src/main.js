c6c12ea2d4207230d67c545618082f92f1abb4df
Myat Thazin Aung <myatthazinaung.mtu@gmail.com>
2023-07-26 12:31:20 +0630

    upload files via update
    create vue+vite

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import router from './router'
 import { createPinia } from 'pinia'
 // FILE: main.js
 
-// import { createApp } from 'vue'
 import { Quasar } from 'quasar'
 
 // Import icon libraries
@@ -15,10 +14,6 @@ import '@quasar/extras/mdi-v7/mdi-v7.css'
 
 // Import Quasar css
 import 'quasar/src/css/index.sass'
-
-// Assumes your root component is App.vue
-// and placed in same folder as main.js
-// import App from './App.vue'
 
 const myApp = createApp(App)
 
