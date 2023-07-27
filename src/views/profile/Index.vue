37d2f60afb82b604772661e85bcc132498b20d9e
Myat Thazin Aung <myatthazinaung.mtu@gmail.com>
2023-07-27 14:17:45 +0630

    upload files via update

--- src/views/profile/Index.vue
+++ src/views/profile/Index.vue
@@ -9,7 +9,19 @@
         </q-breadcrumbs>
     </div>
     <div class="row q-pa-md example-row-equal-width">
-        <div class="col-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam quos doloremque ratione nisi veniam beatae, nemo dolorem molestias reprehenderit. Necessitatibus a eos, mollitia voluptate fugit ipsa amet. Eligendi unde repellat doloribus expedita cum accusamus fugit vero quaerat inventore. Dolorem a nostrum nulla illo vel consectetur minima doloribus minus neque earum quo repudiandae ratione iure sapiente magni rem dignissimos magnam est amet repellendus alias, doloremque incidunt nam! Ullam numquam nihil et necessitatibus repellendus illo eaque, eius recusandae. Beatae perferendis nam suscipit. Cupiditate, veniam! Sed at sint possimus id ea accusantium magnam soluta, eveniet rerum, facere vitae delectus suscipit eum quaerat eaque dolor veritatis, perspiciatis dolorum rem sunt? Optio quibusdam id pariatur sunt ducimus non accusamus, vero autem sapiente maxime reprehenderit qui iste aspernatur in. Impedit pariatur nihil a nostrum esse autem est vel fugit amet dolorum, doloremque eos natus deleniti necessitatibus ab minus nesciunt excepturi ipsum omnis voluptate nisi alias laudantium! Odit maiores ratione tempora architecto dicta! Voluptatibus laboriosam doloremque alias eveniet aliquid pariatur consequuntur ullam! Perspiciatis non ex asperiores quos nihil necessitatibus quisquam? Possimus mollitia odit nemo aut corrupti nostrum tempore eius fugiat ipsum sequi vero itaque ex autem, pariatur voluptatem, ut qui quae reiciendis! Saepe hic id corrupti.</div>
+        <div class="col-3"><q-card class="my-card">
+          <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+    </div>
         <div class="col-9">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam illo quia sit unde provident, temporibus ad modi laudantium aperiam nobis doloribus vitae, excepturi in? Ullam qui, distinctio fuga nesciunt optio ut minus tenetur laudantium vel a odit sapiente. Tenetur adipisci, dolore dicta voluptatum officia qui quia veniam aperiam minus aspernatur quae, ad voluptatibus hic explicabo. Voluptas voluptatum totam magni cumque aliquid beatae! Delectus laudantium quae facere rem! Ex at recusandae impedit rerum magni ad ipsum eligendi quasi omnis? Quia autem libero sint iure maiores animi dolorum delectus dolorem ea tempora. Debitis necessitatibus corrupti iusto pariatur ea, eveniet consequuntur nobis odio.</div>
     </div>
 </template>
